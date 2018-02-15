94c9e03c67da591092f6d2ee28a8c40e994fd900
Jonah Yung <Jonahyung2014@gmail.com>
2018-02-15 01:18:48 +0000

    comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
 
   validates :name, :address, :description, presence: true, length: {minimum: 3}
-end
