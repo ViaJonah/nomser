2e6e1870eda01a84bb2dbe42db262921b007e4c9
Jonah Yung <Jonahyung2014@gmail.com>
2018-04-20 22:25:34 +0000

    Email lesson 42

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,14 +3,14 @@ before_action :authenticate_user!
 
   def create
     @place = Place.find(params[:place_id])
-    @place.photos.create(photos_params)
+    @place.photos.create(photos_params.merge)
     redirect_to place_path(@place), notice: "Photo successfully uploaded"
   end
 
   private
 
   def photos_params
-    params.require(:photos).permit(:caption)
+    params.require(:photo).permit(:caption, :user_id, :place_id)
   end
 
 end
